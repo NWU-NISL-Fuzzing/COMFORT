c8fdcff6f3973afad687f1904a17cbf61c490601
QuXing9 <40267809+QuXing9@users.noreply.github.com>
2021-03-28 13:56:38 +0800

    Create nvidia-container-runtime-script.sh

---
+++ data/nvidia-container-runtime-script.sh
@@ -0,0 +1,8 @@
+sudo curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+sudo curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+sudo apt-get update
+sudo apt-get install nvidia-container-runtime
+systemctl restart docker
