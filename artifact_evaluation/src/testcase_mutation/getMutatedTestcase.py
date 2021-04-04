bf31f36c1a1b3fd3817fbb54096a09e471613b42
Zheng Wang <2528855+zwang4@users.noreply.github.com>
2021-04-04 08:23:07 +0100

    Update getMutatedTestcase.py

--- artifact_evaluation/src/testcase_mutation/getMutatedTestcase.py
+++ artifact_evaluation/src/testcase_mutation/getMutatedTestcase.py
@@ -3,7 +3,7 @@ import execjs
 
 def mutate_testcase(file_path):
     """
-    mutate testcase according ECMAScript-262 10 edition
+    mutate testcase according ECMAScript-262 edition
     :param file_path: the file that contain the testcase and API nodes
     :return: the mutated tastcases
     """
